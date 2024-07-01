3eec2cd54f8dad81a71ed4301c47bcfdb0cb11ad
sidane17 <khalilsidane89@gmail.com>
2024-07-01 02:23:13 +0200

    milstone 2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    $data=[
+        'nome'=>'khalil',
+        'cognome'=>'sidane',
+        'email'=>'khalil@gmail.com'
+    ];
+    return view('welcome',$data);
+});
